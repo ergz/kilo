34054a4e0e0cf16484dcf717d79b7b4ed4627e6f
ergz <e.emanuel.rodriguez@gmail.com>
2021-04-24 20:58:33 -0700

    adds first section and a build script

--- build.bat
+++ build.bat
@@ -1,3 +1,11 @@
 @echo off
 
-gcc kilo.c -o kilo -Wall -Wextra -pedantic -std=c99
+rem when you have a working posix system in windows
+rem gcc kilo.c -o kilo -Wall -Wextra -pedantic -std=c99
+
+rem, build from wsl
+rem location of the wsl bash
+set BASH=C:\Windows\System32\bash.exe
+set WSL_BASH=C:\Windows\System32\wsl.exe -d Ubuntu-20.04 -e
+
+%BASH% -c "gcc -g -Wall -Wextra -pedantic -std=c99 kilo.c -o kilo.exe"
